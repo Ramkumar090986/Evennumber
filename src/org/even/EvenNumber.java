c8d174bcda25771dc867ca2e7cb4c5aa834bb00e
Ramkumar090986 <ramkumar8687@gmail.com>
2020-11-24 17:23:38 +0530

    Even number

---
+++ src/org/even/EvenNumber.java
@@ -0,0 +1,26 @@
+package org.even;
+
+import java.util.Scanner;
+
+public class EvenNumber {
+	public static void main(String[] args) {
+		Scanner s1=new Scanner(System.in);
+		
+		System.out.println("Enter the Even number:");
+		int i=s1.nextInt();
+		if (i%2==0) {
+			System.out.println("The entered number is even number:"+i);
+			
+		} else {
+			System.out.println("The entered number is not even number:"+i);
+
+		}
+}
+}
+		
+		
+		
+	
+	
+
+
---
+++ src/org/even/Evenorodd.java
@@ -0,0 +1,17 @@
+package org.even;
+
+public class Evenorodd {
+	
+	public static void main(String[] args) {
+		//int count=0;
+		for (int i = 0; i <=100; i++) {
+			if (i%2==1) {
+				System.out.println(i);
+				
+			} 
+			
+		}
+		
+	}
+
+}
