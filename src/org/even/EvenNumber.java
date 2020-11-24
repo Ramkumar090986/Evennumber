e516a114d16a67c09e3b30bb1d23510547048fc6
Ramkumar090986 <ramkumar8687@gmail.com>
2020-11-24 17:39:21 +0530

    Even1 method added

--- src/org/even/EvenNumber.java
+++ src/org/even/EvenNumber.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class EvenNumber {
 	public static void main(String[] args) {
+		
 		Scanner s1=new Scanner(System.in);
 		
 		System.out.println("Enter the Even number:");
@@ -15,7 +16,12 @@ public class EvenNumber {
 			System.out.println("The entered number is not even number:"+i);
 
 		}
+
 }
+	public void testmethod() {
+		System.out.println("Test Method");
+
+	}
 }
 		
 		
